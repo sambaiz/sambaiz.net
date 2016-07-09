76b5e74c4d92972b23d7fbbc6bc81a5fdec042c5
sambaiz <godgourd@gmail.com>
2016-07-10 03:25:35 +0900

    url encode

--- web/js/components/ArticleDetailPage/Share.jsx
+++ web/js/components/ArticleDetailPage/Share.jsx
@@ -12,9 +12,9 @@ export default class Share extends Component {
         <div className={styles.share}>シェアボタン</div>
         <ul className={classNames(styles.circle_group, styles.clearfix)}>
           <li className={classNames(styles.sns_circle, styles.hatebu)}><span><i className={classNames("fa", styles["fa-hatena"])}></i></span><a data-hatena-bookmark-title={title} href={`http://b.hatena.ne.jp/entry/${url}`}></a></li>
-          <li className={classNames(styles.sns_circle, styles.twitter)}><span><i className={classNames("fa", "fa-twitter")}></i></span><a href={`https://twitter.com/share?url=${url}&amp;text=${title}`}></a></li>
-          <li className={classNames(styles.sns_circle, styles.googleplus)}><span><i className={classNames("fa", "fa-google-plus")}></i></span><a href={`https://plus.google.com/share?hl=ja&amp;url=${url}`}></a></li>
-          <li className={classNames(styles.sns_circle, styles.pocket)}><span><i className={classNames("fa", "fa-get-pocket")}></i></span><a href={`http://getpocket.com/edit?url=${url}&amp;title=${title}`}></a></li>
+          <li className={classNames(styles.sns_circle, styles.twitter)}><span><i className={classNames("fa", "fa-twitter")}></i></span><a href={`https://twitter.com/share?url=${url}&amp;text=${encodeURIComponent(title)}`}></a></li>
+          <li className={classNames(styles.sns_circle, styles.googleplus)}><span><i className={classNames("fa", "fa-google-plus")}></i></span><a href={`https://plus.google.com/share?hl=ja&amp;url=${encodeURIComponent(url)}`}></a></li>
+          <li className={classNames(styles.sns_circle, styles.pocket)}><span><i className={classNames("fa", "fa-get-pocket")}></i></span><a href={`http://getpocket.com/edit?url=${url}&amp;title=${encodeURIComponent(title)}`}></a></li>
         </ul>
       </div>
     )
